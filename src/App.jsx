902a2cbc9c8ad32045c45c7f0db17c6c347c24b7
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-05-11 00:22:58 +0530

    Input for FCFS have been implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header'
+import Input from './components/Input'
 import { ThemeProvider } from './context/ThemeContext'
 
 function App() {
   return (
     <ThemeProvider>
       <Header />
+      <Input />
     </ThemeProvider>
         
   )
